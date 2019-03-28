529e72640839dcf97b9e7bdcb8c346cd0674c35a
lyc10031 <32125389+lyc10031@users.noreply.github.com>
2019-03-28 16:03:09 +0800

    get current script pid and kill it
    use command 
    kill -9 $id
    shutdown the control.py

--- kill.sh
+++ kill.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # shut down ths script use kill 
-id=`ps -h | grep -i "control.py" | awk -F ' ' 'NR==1 { print $1 }'`
+id=`cat PID.txt`
+echo -e " \n The process $id will be killed!!!\n"
+
 kill -9 $id
 
