2507e5be6c7a94101196df36da674816495dcde0
lyc10031 <32125389+lyc10031@users.noreply.github.com>
2019-05-10 10:55:47 +0800

    read alllogfile

---
+++ get_report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $# == 1 ];then
+for i in `ls $1/*.txt`
+do
+awk -F ' ' '{print $4,"<->",$5,$6 }' $i  | grep OK | tail -n 1
+done
+
+else
+
+for i in `ls run_result/*.txt`
+do
+awk -F ' ' '{print $4,"<->",$5,$6 }' $i  | grep OK | tail -n 1
+done
+
+fi
